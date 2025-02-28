65096708b4ea9a6cead0dddce397ec81743f9eab
hildanku <hildankutomo@gmail.com>
2025-03-01 00:08:52 +0700

    feat(schema): add email column to users table

--- src/config/db/schema.ts
+++ src/config/db/schema.ts
@@ -14,6 +14,7 @@ export const userTable = mysqlTable('users', {
         .default('operator')
         .notNull(),
     avatar: text(),
+    email: text().notNull(),
 })
 
 export type User = typeof userTable.$inferSelect
