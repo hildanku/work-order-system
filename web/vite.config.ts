f23ef769570c2cc8046f2968100a06538f6679c6
hildanku <hildankutomo@gmail.com>
2025-03-02 20:27:27 +0700

    fix(vite.config): fix proxy config to route API req to backend - add lib tanstack/svelte-query, icon lucide-svelte - add aliases @root

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+const PROD = import.meta.env?.PROD ?? false
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+        port: 5173,
+        proxy: {
+            '/api': {
+                target: PROD ? '/' : 'http://localhost:1337',
+                changeOrigin: true,
+                secure: false, // if backend not use https protocol
+                rewrite: (path) => path.replace(/^\/api/, '/api')
+            },
+            '/public': {
+                target: PROD ? '/' : 'http://localhost:1337',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/public/, '/public')
+            }
+        }
+    }
 });
