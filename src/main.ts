7bf143d421b6642d46201c4e58512b5b4373dabf
hildanku <hildankutomo@gmail.com>
2025-03-07 01:28:04 +0700

    feat(main): make backend can serve static file - ref: https://hono.dev/docs/getting-started/bun#serve-static-files

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { logger } from 'hono/logger'
 import { healthController } from "./modules/health/health.controller";
 import { userController } from './modules/user/user.controller'
 import { productController } from "./modules/product/product.controller";
+import { serveStatic } from 'hono/bun'
 
 const route = new Hono()
     .route('/auth', authenticationController)
@@ -14,6 +15,14 @@ const route = new Hono()
 const app = new Hono()
     .use('*', logger())
     .route('/api', route)
+    .use(
+        '/public/*',
+        serveStatic({
+            root: './src/public',
+            rewriteRequestPath: (path) =>
+                path.replace('/public', ''),
+        })
+    )
 
 const port = 1337
 
